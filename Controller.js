95c2338230403ae9712f22627f0174bd468d2f5c
victor-singha <vicunite2@gmail.com>
2021-06-05 14:56:15 +0530

    add food

--- Controller.js
+++ Controller.js
@@ -1,4 +1,25 @@
 const Order = require("./Order");
+const Food = require("./Food");
 
-function newOrder() {}
+var allOrder = [];
+
+function newOrder() {
+  const customer_name = prompt("Customer Name: ");
+  var foodList = [];
+  var isFood = true;
+  while (isFood) {
+    const food_name = prompt("Food? : ");
+    const food_price = prompt("Price? : ");
+    if (food_name != "") {
+      const food = new Food(food_name, food_price);
+      foodList.push(food);
+    } else isFood = false;
+  }
+
+  const order = new Order();
+  order.orderID = 424;
+  order.customer = customer_name;
+  order.food = foodList;
+  order.totalprice = 999;
+}
 function showOrders() {}
